dcdae98c99b1daa94a5e8f49f8d5241716640ca7
truple98 <ljw122321@gmail.com>
2025-08-29 17:23:38 +0900

    init vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false
-    },
-    fs: {
-      allow: ['..']
-    }
-  }
-});
+})
